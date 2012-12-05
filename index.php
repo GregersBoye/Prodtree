322ed98dc496dda617f8401af72911f5d5e33400
Gregers Boye-Jacobsen <gregers.boye@gmail.com>
2012-12-05 22:38:12 +0100

    Første setup
    filstruktur sat op, indledende javascript lavet - meget primitiv graf
    med opskrifter

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	
+	<title>Production tree</title>
+
+</head>
+<body>
+Følgende opskrifter er tilgængelige:
+<ul id="products">
+</ul>
+Skriv showRessources('[opskrift]') i konsollen og se opskriften.
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,58 @@
+products= {
+		//Forbrugsvarer:
+		"pil":['lvl 1 smed', 'lvl 1 tømrer', 'jernmalm', 'småpinde'],
+		"lokkemad": ['lvl 1 kroejer', 'harekød', 'grøntsager'],
+		"patron": ['lvl 1 smed', 'blymalm'],
+		'giftpil': ['lvl 1 smed', 'lvl 2 læge', 'blymalm', 'svovl'],
+		'rebpil':['lvl 2 smed', 'lvl 1 skrædder', 'koskind'],
+		'røgbombe':['lvl 3 smed', 'krudt', 'ler', 'farver'],
+		'snare': ['lvl 2 tømrer', 'koskind', 'småpinde'],
+		'snublemine':['lvl 4 smed', 'krudt', 'ler', 'flint'],
+		//Mad:
+		'brød': ['lvl 1 kroejer', 'æg', 'mel', 'mælk'],
+		'tørret hjortekød': ['lvl 1 kroejer', 'groft salt', 'rådyrkød'],
+		'hjortemarvssuppe': ['lvl 1 kroejer', 'hjortemarv', 'ferskvand', 'grøntsager'],
+		'kødtærter': ['lvl 1 kroejer', 'mel', 'harekød'],
+		'salt': ['lvl 1 kroejer', 'groft salt'],
+		'saltet hjortekød': ['lvl 1 kroejer', 'hjortekød', 'groft salt'],
+		'sheperd\'s pie': ['lvl 1 kroejer', 'svinekød', 'fuglekød', 'grøntsager'],
+		'Røget elgkød': ['lvl 1 kroejer', 'elgkød', 'ahorn tømmer'],
+		
+		
+		//alkohol:
+		'øl': ['lvl 1 kroejer', 'byg', 'tønder'],
+		'cider': ['lvl 1 kroejer', 'æbler', 'tønder'],
+		"spiritus": ['lvl 1 kroejer', 'tønder', 'rug'],
+		//Materialer:
+		'tønder': ['lvl 1 tømrer', 'egetræ'],
+               
+};
+function showRessources(product){
+	var item = products[product];
+	
+	if(!products.hasOwnProperty(product)){
+		console.log(product);
+	}else{
+		console.log(product+":");
+		for(var i in item){
+			
+			showRessources(item[i]);
+		}
+		
+	}
+		
+	
+}
+$('document').ready(function(){
+
+for(var i in  products){
+	$('#products').append('<li>'+i+'</li>');
+}
+
+$('#products li').on('click', function(){
+	var recipe = $(this).text();
+	showRessources(recipe);
+})
+
+
+})
