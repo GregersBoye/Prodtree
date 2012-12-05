d8dc76ac4a82f9d9777275f490cad8b58035d456
Gregers Boye-Jacobsen <gregers.boye@gmail.com>
2012-12-05 22:57:45 +0100

    Further localization
    Forgot to save some files - my bad.

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
-products= {
-		//Forbrugsvarer:
-		"pil":['lvl 1 smed', 'lvl 1 tømrer', 'jernmalm', 'småpinde'],
-		"lokkemad": ['lvl 1 kroejer', 'harekød', 'grøntsager'],
-		"patron": ['lvl 1 smed', 'blymalm'],
-		'giftpil': ['lvl 1 smed', 'lvl 2 læge', 'blymalm', 'svovl'],
-		'rebpil':['lvl 2 smed', 'lvl 1 skrædder', 'koskind'],
-		'røgbombe':['lvl 3 smed', 'krudt', 'ler', 'farver'],
-		'snare': ['lvl 2 tømrer', 'koskind', 'småpinde'],
-		'snublemine':['lvl 4 smed', 'krudt', 'ler', 'flint'],
-		//Mad:
-		'brød': ['lvl 1 kroejer', 'æg', 'mel', 'mælk'],
-		'tørret hjortekød': ['lvl 1 kroejer', 'groft salt', 'rådyrkød'],
-		'hjortemarvssuppe': ['lvl 1 kroejer', 'hjortemarv', 'ferskvand', 'grøntsager'],
-		'kødtærter': ['lvl 1 kroejer', 'mel', 'harekød'],
-		'salt': ['lvl 1 kroejer', 'groft salt'],
-		'saltet hjortekød': ['lvl 1 kroejer', 'hjortekød', 'groft salt'],
-		'sheperd\'s pie': ['lvl 1 kroejer', 'svinekød', 'fuglekød', 'grøntsager'],
-		'Røget elgkød': ['lvl 1 kroejer', 'elgkød', 'ahorn tømmer'],
-		
-		
-		//alkohol:
-		'øl': ['lvl 1 kroejer', 'byg', 'tønder'],
-		'cider': ['lvl 1 kroejer', 'æbler', 'tønder'],
-		"spiritus": ['lvl 1 kroejer', 'tønder', 'rug'],
-		//Materialer:
-		'tønder': ['lvl 1 tømrer', 'egetræ'],
-               
-};
+
 function showRessources(product){
 	var item = products[product];
 	
